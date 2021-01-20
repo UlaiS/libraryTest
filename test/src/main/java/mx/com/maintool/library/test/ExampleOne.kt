fe26df2121632555ed5f720939dfde7d1e941365
Ulain <ulai.nava@cargamos.com>
2021-01-20 12:38:39 -0600

    first commit

---
+++ test/src/main/java/mx/com/maintool/library/test/ExampleOne.kt
@@ -0,0 +1,10 @@
+package mx.com.maintool.library.test
+
+class ExampleOne {
+    fun methodSum(n1: Int, n2: Int): Int{
+        return n1 + n2
+    }
+    fun methosMinus(n1: Int, n2: Int): Int{
+        return n1 + n2
+    }
+}
